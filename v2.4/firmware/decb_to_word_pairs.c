205b1100c70b7954876fad7256a6f3fc17c74a45
Henry Strickland <strick@yak.net>
2025-03-17 16:54:43 -0700

    Made LIFE load and run with C Firmware.
    using firmware/c/bonobo.cpp & firmware/life-semi.c

--- v2.4/firmware/decb_to_word_pairs.c
+++ v2.4/firmware/decb_to_word_pairs.c
@@ -9,7 +9,9 @@ typedef unsigned char byte;
 
 byte q[5];  // 5 byte quintuple
 
-int main() {
+
+
+void ForPython() {
   printf("PairsOfWords = [\n");
   while (1) {
     if (q[0]) break; // not 00, so must be FF with JMP address.
@@ -38,4 +40,43 @@ int main() {
       0, q[4], q[3], JMP_EXTENDED,
       (q[3]<<8)+q[4]);
   printf("]\n");
+}
+void ForC() {
+  printf("unsigned int const PairsOfWords[] = {\n");
+  while (1) {
+    if (q[0]) break; // not 00, so must be FF with JMP address.
+
+    fread((char*)q, sizeof *q, sizeof q, stdin);
+    unsigned n = ((unsigned)q[1]<<8) | q[2];
+    unsigned base = ((unsigned)q[3]<<8) | q[4];
+
+    for (unsigned i = 0; i<n; i++) {
+      byte a = (byte) getchar();
+      unsigned b1 = a;
+      unsigned b2 = 0;  // just one byte at a time, the first
+
+      unsigned a1 = 255&((base+i)>>8);
+      unsigned a2 = 255&((base+i)>>0);
+
+      printf("    0x%02x%02x%02x%02x, 0x%02x%02x%02x%02x,  //  LDD #$%04x ; STD $%04x\n",
+          b2, b1, LDD_IMMEDIATE, 0xFF,
+          0, a2, a1, STD_EXTENDED,
+	      (b1<<8)+b2,
+	      (a1<<8)+a2);
+    }
+  }
+
+  printf("    0x%02x%02x%02x%02x, 0x%02x%02x%02x%02x,  //  LDD #$0000 ; JMP $%04x\n",
+      0, 0, LDD_IMMEDIATE, 0xFF,
+      0, q[4], q[3], JMP_EXTENDED,
+      (q[3]<<8)+q[4]);
+
+  printf("};\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc==2 && argv[1][0] == 'c')
+        ForC();
+    else
+        ForPython();
 }
